51275a77db810b9582ee2293ef85cde1ef4cd33e
Nan Jiang <jiang631@purdue.edu>
2024-04-23 00:46:40 -0400

    update script

--- act_dso/scripts/sindy_data/run.lorenz.xuelab.sh
+++ act_dso/scripts/sindy_data/run.lorenz.xuelab.sh
@@ -4,16 +4,16 @@ py3=/home/$USER/miniconda3/envs/py310/bin/python3.10
 #
 type=Lorenz
 #datapath=$basepath/data/differential_equations/
-opt=L-BFGS-B
+opt=Nelder-Mead
 noise_type=normal
 noise_scale=0.0
-metric_name=inv_mse
+metric_name=neg_mse
 n_cores=1
 batch_size=5
 set -x
 for eq_name in Lorenz;
 do
-  echo "submit $eq_name"
+	echo "submit $eq_name"
 
 	dump_dir=$basepath/result/${type}/$(date +%F)
 	echo $dump_dir
@@ -21,8 +21,6 @@ do
 		echo "create output dir: $dump_dir"
 		mkdir -p $dump_dir
 	fi
-	for bsl in DSR; do
-		CUDA_VISIBLE_DEVICES=""  $py3 $basepath/act_dso/main.py $basepath/act_dso/config/config_regression_${bsl}.json --equation_name $eq_name \
-			--optimizer $opt --metric_name $metric_name --batch_size $batch_size --noise_type $noise_type --noise_scale $noise_scale  --n_cores $n_cores #>$dump_dir/Eq_${eq_name}.noise_${noise_type}${noise_scale}.opt$opt.${bsl}.cvdso.out &
+	CUDA_VISIBLE_DEVICES=""  $py3 $basepath/act_dso/main.py $basepath/act_dso/config/config_regression_DSR.json --equation_name $eq_name \
+		--optimizer $opt --metric_name $metric_name --batch_size $batch_size --noise_type $noise_type --noise_scale $noise_scale  --n_cores $n_cores  >$dump_dir/Eq_${eq_name}.noise_${noise_type}${noise_scale}.opt$opt.act_dso.out &
 	done
-done
