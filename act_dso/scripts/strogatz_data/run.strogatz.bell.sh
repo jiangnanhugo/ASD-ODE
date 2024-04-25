e74954d86cf3cadd6c5aede1831aaa9b952c7b1d
Nan Jiang <jiang631@purdue.edu>
2024-04-25 01:46:03 -0400

    update

--- act_dso/scripts/strogatz_data/run.strogatz.bell.sh
+++ act_dso/scripts/strogatz_data/run.strogatz.bell.sh
@@ -8,7 +8,7 @@ opt=Nelder-Mead
 noise_type=normal
 noise_scale=0.0
 metric_name=neg_mse
-n_cores=10
+n_cores=8
 num_init_conds=5
 nvars=$1
 total_progs=$2
@@ -16,13 +16,33 @@ for ei in {1..${total_progs}};
 do
   eq_name=${nvars}_prog${ei}
 	echo "submit $eq_name"
-
+  log_dir=$basepath/log/$(date +%F)
+	echo $log_dir
+	if [ ! -d "$log_dir" ]; then
+		echo "create dir: $log_dir"
+		mkdir -p $log_dir
+	fi
 	dump_dir=$basepath/result/${type}/$(date +%F)
 	echo $dump_dir
 	if [ ! -d "$dump_dir" ]; then
 		echo "create output dir: $dump_dir"
 		mkdir -p $dump_dir
 	fi
-	CUDA_VISIBLE_DEVICES=""  $py3 $basepath/act_dso/main.py $basepath/act_dso/config/config_regression.json --equation_name $eq_name \
+	sbatch -A yexiang --nodes=1 --ntasks=1 --cpus-per-task=${n_cores} <<EOT
+#!/bin/bash -l
+
+#SBATCH --job-name="ODE-V${nvars}-Pg${total_progs}"
+#SBATCH --output=$log_dir/${eq_name}.noise_${noise_type}_${noise_scale}.opt$opt.act_dso.out
+#SBATCH --constraint=A
+#SBATCH --time=12:00:00
+#SBATCH --mem=8GB
+
+hostname
+
+$py3 $basepath/act_dso/main.py $basepath/act_dso/config/config_regression.json --equation_name $eq_name \
 		--optimizer $opt --metric_name $metric_name --num_init_conds $num_init_conds --noise_type $noise_type --noise_scale $noise_scale  --n_cores $n_cores  >$dump_dir/Eq_${eq_name}.noise_${noise_type}${noise_scale}.opt$opt.act_dso.out &
+
+EOT
+	done
+done
 	done
