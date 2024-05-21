7f8dec471f704a139e59af8bbdcc70fad3b21a9e
Nan Jiang <jiang631@purdue.edu>
2024-05-21 17:28:15 -0400

    update name

--- baselines/SPL/scripts/odebase_data/onekey.sh
+++ baselines/SPL/scripts/odebase_data/onekey.sh
@@ -1,10 +1,10 @@
-./run.strogatz.anvil.sh vars1 27
-./run.strogatz.anvil.sh vars2 37
-./run.strogatz.anvil.sh vars3 30
-./run.strogatz.anvil.sh vars4 19
+./run.odebase.anvil.sh vars1 27
+./run.odebase.anvil.sh vars2 37
+./run.odebase.anvil.sh vars3 30
+./run.odebase.anvil.sh vars4 19
 
 
-./run.strogatz.noisy.anvil.sh vars1 27
-./run.strogatz.noisy.anvil.sh vars2 37
-./run.strogatz.noisy.anvil.sh vars3 30
-./run.strogatz.noisy.anvil.sh vars4 19
+./run.odebase.noisy.anvil.sh vars1 27
+./run.odebase.noisy.anvil.sh vars2 37
+./run.odebase.noisy.anvil.sh vars3 30
+./run.odebase.noisy.anvil.sh vars4 19
