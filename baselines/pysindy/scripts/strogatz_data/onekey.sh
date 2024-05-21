bf5086f2ea9aefc15d40472c0d4b26491bdfe1aa
Nan Jiang <jiang631@purdue.edu>
2024-05-21 01:46:13 -0400

    update

--- baselines/pysindy/scripts/strogatz_data/onekey.sh
+++ baselines/pysindy/scripts/strogatz_data/onekey.sh
@@ -1,2 +1,4 @@
-./run.strogatz.xuelab.sh vars1 23
-./run.strogatz.xuelab.sh vars2 28
+./run.strogatz.anvil.sh vars1 23
+./run.strogatz.anvil.sh vars2 27
+./run.strogatz.anvil.sh vars3 19
+./run.strogatz.anvil.sh vars4 10
