8dc4e2c8636b1fbfcb27ccd3f18348bda80aa91e
Nan Jiang <jiang631@purdue.edu>
2024-05-22 16:11:43 -0400

    update

--- baselines/pysindy/scripts/odebase/onekey.sh
+++ baselines/pysindy/scripts/odebase/onekey.sh
@@ -18,7 +18,7 @@
 ./run.odebase.irre.anvil.sh vars4 19
 
 #$$$$
-./run.odebase.desktop.sh vars1 27
-./run.odebase.desktop.sh vars2 37
-./run.odebase.desktop.sh vars3 30
-./run.odebase.desktop.sh vars4 19
+./run.odebase.desktop.sh vars2 27
+./run.odebase.desktop.sh vars3 37
+./run.odebase.desktop.sh vars4 30
+./run.odebase.desktop.sh vars5 19
--- baselines/pysindy/scripts/odebase/run.odebase.desktop.sh
+++ baselines/pysindy/scripts/odebase/run.odebase.desktop.sh
@@ -13,7 +13,7 @@ nvars=$1
 total_progs=$2
 set -x
 for ei in {1..${total_progs}}; do
-	eq_name=${nvars}_prog${ei}
+	eq_name=${type}_${nvars}_prog${ei}
 	echo "submit $eq_name"
 
 	dump_dir=$basepath/result/${type}/$(date +%F)
