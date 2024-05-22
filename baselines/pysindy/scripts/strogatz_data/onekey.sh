276ad6d03a22ea5e8118f6d432057754189e20ce
Nan Jiang <jiang631@purdue.edu>
2024-05-22 13:33:52 -0400

    add more commands

--- baselines/pysindy/scripts/strogatz_data/onekey.sh
+++ baselines/pysindy/scripts/strogatz_data/onekey.sh
@@ -2,3 +2,16 @@
 ./run.strogatz.anvil.sh vars2 27
 ./run.strogatz.anvil.sh vars3 19
 ./run.strogatz.anvil.sh vars4 10
+
+
+
+./run.strogatz.noisy.anvil.sh vars1 23
+./run.strogatz.noisy.anvil.sh vars2 27
+./run.strogatz.noisy.anvil.sh vars3 19
+./run.strogatz.noisy.anvil.sh vars4 10
+
+
+./run.strogatz.irre.anvil.sh vars1 23
+./run.strogatz.irre.anvil.sh vars2 27
+./run.strogatz.irre.anvil.sh vars3 19
+./run.strogatz.irre.anvil.sh vars4 10
