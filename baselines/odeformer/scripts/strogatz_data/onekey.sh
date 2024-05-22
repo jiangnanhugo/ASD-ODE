89e7284f33f6d94c6d7ee3729ddebe8d18f6483a
Nan Jiang <jiang631@purdue.edu>
2024-05-22 04:24:02 -0400

    update

--- baselines/odeformer/scripts/strogatz_data/onekey.sh
+++ baselines/odeformer/scripts/strogatz_data/onekey.sh
@@ -9,3 +9,9 @@
 ./run.strogatz.noisy.anvil.sh vars2 27
 ./run.strogatz.noisy.anvil.sh vars3 19
 ./run.strogatz.noisy.anvil.sh vars4 10
+
+
+./run.strogatz.irre.anvil.sh vars1 23
+./run.strogatz.irre.anvil.sh vars2 27
+./run.strogatz.irre.anvil.sh vars3 19
+./run.strogatz.irre.anvil.sh vars4 10
