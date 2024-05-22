6fe157d2a6867221e50184b0e9efc638c2e1baa7
Nan Jiang <jiang631@purdue.edu>
2024-05-22 00:27:51 -0400

    update

--- ablation_study/plot_time_curve.py
+++ ablation_study/plot_time_curve.py
@@ -28,12 +28,12 @@ sns.set_style("ticks")
 
 mpl.rcParams['lines.markersize'] = 6
 mpl.rcParams['lines.linewidth'] = 1
-rc("font", **{'family': 'serif', 'serif': ['Palatino'], 'size': 12})
+rc("font", **{'family': 'serif', 'serif': ['Palatino'], 'size': 14})
 rc('text', usetex=True)
 plt.style.use("seaborn-v0_8-bright")
 
 noise_type = 'normal'
-noise_scale = 0.05
+noise_scale = 0.1
 metric_name = 'neg_mse'
 time_sequence_drop_rate = 0.5
 
@@ -52,7 +52,7 @@ def irregular_time_squence(true_trajectories, time_sequence, time_sequence_drop_
     return masked_true_traj, masked_time_sequence
 
 
-def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
+def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test", loc='lower right'):
     # equation_name = f"vars2_prog{eq_id}"
     data_query_oracle = Equation_evaluator(equation_name,
                                            noise_type, noise_scale,
@@ -64,7 +64,7 @@ def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
     # function_set = data_query_oracle.get_operators_set()
 
     time_span = (0.0, 10)
-    trajectory_time_steps = 100
+    trajectory_time_steps = 300
     num_init_conds = 1
     t_eval = np.linspace(time_span[0], time_span[1], trajectory_time_steps)
     num_regions = 10
@@ -75,9 +75,12 @@ def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
                        time_span, t_eval,
                        num_of_regions=num_regions,
                        width=0.1)
-    if init_conds is not None:
+    if init_conds is None:
         init_conds = task.rand_draw_init_cond()
     else:
+        # print(task.rand_draw_init_cond().shape)
+        # print(init_conds.shape)
+        init_conds = init_conds.reshape(1, nvars)
         task.init_cond = init_conds
     true_trajectories = task.evaluate()[0]
     irregular_true_trajectories, irregular_time = irregular_time_squence(true_trajectories,
@@ -89,16 +92,17 @@ def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
     print(t_eval.shape)
     print(irregular_true_trajectories.shape)
     print(irregular_time.shape)
-    plt.figure(figsize=(4, 3))
+    plt.figure(figsize=(3.5, 3.5))
     for dim in range(nvars):
-        plt.plot(t_eval, pred_trajectories[:, dim], color=f'C{dim}', alpha=.2, lw=10, label='predicted '+r"$x_{}$".format(dim))
-        plt.plot(irregular_time, irregular_true_trajectories[:, dim], ls="None", marker='o', alpha=.3, color=f'C{dim}',
-                 label='True '+r"$x_{}$".format(dim))
+        plt.plot(t_eval, pred_trajectories[:, dim], color=f'C{dim}', alpha=.2, lw=5,
+                 label='Predicted ' + r"$x_{}$".format(dim + 1))
+        plt.plot(irregular_time, irregular_true_trajectories[:, dim], ls="None", marker='.', alpha=.3, color=f'C{dim}',
+                 label='True ' + r"$x_{}$".format(dim + 1))
     plt.grid(False)
-    plt.xlabel("Time Sequence (second)")
-    plt.ylabel("Variable Value")
-    plt.legend(loc='best')
-    legend = plt.legend()
+    plt.xlabel("Time Sequence (second)", fontsize=16)
+    plt.ylabel("Variable Value", fontsize=16)
+    # plt.legend(loc='upper right')
+    legend = plt.legend(loc=loc)
 
     # Remove the boundary and background color
     legend.get_frame().set_edgecolor('none')
@@ -115,7 +119,7 @@ def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
     # title = f"Equation ID {i}"
     # # + ": " + true_equation._description
     # print(title)
-    plt.title(title, fontsize=11)
+    plt.title(title, fontsize=16)
     # # plt.show()
     # #
     fname = os.path.join(name + "_time_seq.pdf")
@@ -123,11 +127,32 @@ def plot(equation_name, our_predict_eq, init_conds=None, title="", name="test"):
     plt.close()
 
 
-name = "tes1"
-plot('vars2_prog5', ['1.04*X1', '-0.02-0.77*X0'], np.array([3, -0.5]),
-     title=r'$\phi=(1.04x_2, -0.02-0.77x_1)$',
+name = "odeformer.at01"
+plot('vars2_prog5', ['1.04*X1', '-0.02-0.77*X0'], np.array([0, 1]),
+     title='ODEFormer Prediction\n' + r'$\phi=(1.04x_2, -0.02-0.77x_1)$',
      name=name)
-name = 'test2'
-plot('vars2_prog5', ['10*sin(X1)', '4*cos(X0+2)'], np.array([3, -0.5]),
-     title=r'$\phi=(10\sin(x_2), 4\cos(x_1+2)$',
+name = 'apps.at01'
+plot('vars2_prog5', ['X1', '-0.895*sin(X0)'], np.array([0, 1]),
+     title='APPS (our) Prediction\n' + r'$\phi=(x_2, 0.895\sin(x_1))$',
      name=name)
+
+name = "odeformer.at4neg1"
+plot('vars2_prog5', ['1.04*X1', '-0.02-0.77*X0'], np.array([4, -1]),
+     title='ODEFormer Prediction\n' + r'$\phi=(1.04x_2, -0.02-0.77x_1)$',
+     name=name, loc= 'lower left',)
+name = 'apps.at4neg1'
+plot('vars2_prog5', ['X1', '-0.895*sin(X0)'], np.array([4, -1]),
+     title='APPS (our) Prediction\n' + r'$\phi=(x_2, 0.895\sin(x_1))$',
+     name=name, loc= 'lower left',)
+
+# def pred_by_odeformer(x_0, x_1):
+#     return  0.9846 * x_1, -0.7883 * x_0
+# name = "test5"
+# plot('vars2_prog5', ['0.9846*X1', '-0.7883*X0'], np.array([0, 1]),
+#      title='ODEFormer Prediction '+r'$\phi=(0.9846x_2, -0.7883x_1)$',
+#      name=name)
+#
+# name = "test6"
+# plot('vars2_prog5', ['0.9846*X1', '-0.7883*X0'], np.array([4, -1]),
+#      title='ODEFormer Prediction '+r'$\phi=(0.9846x_2, -0.7883x_1)$',
+#      name=name)
--- apps_ode_pytorch/scripts/odebase_data/onekey.sh
+++ apps_ode_pytorch/scripts/odebase_data/onekey.sh
@@ -12,7 +12,7 @@
 
 
 
-./run.strogatz.irregular.anvil.sh vars1 27
-./run.strogatz.irregular.anvil.sh vars2 37
-./run.strogatz.irregular.anvil.sh vars3 30
-./run.strogatz.irregular.anvil.sh vars4 19
+./run.odebase.irregular.anvil.sh vars1 27
+./run.odebase.irregular.anvil.sh vars2 37
+./run.odebase.irregular.anvil.sh vars3 30
+./run.odebase.irregular.anvil.sh vars4 19
